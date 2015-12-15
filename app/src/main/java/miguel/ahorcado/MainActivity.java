7ad954741406bb5279ab779b78606d616b0fcb6f
omdiazccc <omdiazc@unal.edu.co>
2015-12-15 18:26:19 -0500

    Commit 18:26

--- app/src/main/java/miguel/ahorcado/MainActivity.java
+++ app/src/main/java/miguel/ahorcado/MainActivity.java
@@ -4,27 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createbuttons();
+
     }
 
-    @Override
-    public void onClick(View v) {
+    private void createbuttons() {
+        // metodo dedicado a definir los botones y su comportamiento
+        btnGame=(Button)findViewById(R.id.btnMain_jugar);
 
-        switch (v.getId()){
 
-            case R.id.btnMain_jugar:
+
+
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 openNewActivity(v, pages.GAME);
-
-
-        }
+            }
+        });
 
     }
+
+
+
 
     private void openNewActivity(View v , pages p){
         switch (p){
@@ -43,7 +52,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     }
 
-
+    private Button btnGame;
     private enum pages {
     GAME;
     }
